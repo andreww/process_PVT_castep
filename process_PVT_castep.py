b282fa937a2ea33f2a733a291adb84a0b07f3a93
Andrew Walker <andrew.walker@bristol.ac.uk>
2014-03-28 09:59:34 +0000

    Clean up Castep reading parts, add static EOS
    Allow 0T EOS and 'static' EOS to be calculated.

--- process_PVT_castep.py
+++ process_PVT_castep.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
-"""Fit F(V,T) data to find EOS parameters from Castep"""
-import re
+"""Fit F(V,T) data to find EOS parameters from Castep
 
+   A Castep 'thermodynamcs' run produces a table of 
+   thermodynamic data from a lattice dynamics calculation.
+   If a serise of such calculations are done for stuctures
+   optimised at different hydrostatic pressures, this information
+   can be used extract the free energy and volume as a function
+   of pressure and temperature in the 'statically constrained'
+   quasi-harmonic approximation (SC QHA). That is, we assume that
+   the phonon freequencies are only a function of the unit cell 
+   volume, and are not directly altered by temperature (via 
+   the anharmonic nature of atomic vibrations) and, furthermore, 
+   that a static cell optimisation gives the cell parameters and
+   internal structure appropreate at some higher temperature and
+   lower pressure.
+
+   This script provides tools to fit Castep thermodynamics data
+   to a set of isothermal EOS and thus find V(P,T). """
+import re
 import numpy as np
 import scipy.optimize as spopt
 
-# extract to do the fitting
-# and some graphing
-
-vol_re = re.compile(r'Current cell volume =\s+(\d+\.\d+)\s+A\*\*3')
-zpe_re = re.compile(r'Zero-point energy =\s+(\d+\.\d+)\s+eV')
-tmo_re = re.compile(
+# Some regular expressions that get use a lot,
+# so we compile them when the module is loaded
+_vol_re = re.compile(r'Current cell volume =\s+(\d+\.\d+)\s+A\*\*3')
+_zpe_re = re.compile(r'Zero-point energy =\s+(\d+\.\d+)\s+eV')
+_tmo_re = re.compile(
    r'(\d+\.\d+)\s+(\d+\.\d+)\s+([+-]?\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)')
-ufb_re = re.compile(
+_ufb_re = re.compile(
    r'Total energy corrected for finite basis set =\s+([+-]?\d+\.\d+)\s+eV')
 
 def parse_castep_file(filename, current_data=[]):
@@ -41,17 +56,17 @@ def parse_castep_file(filename, current_data=[]):
         if skip_lines > 0:
             skip_lines = skip_lines - 1
             continue
-        match = vol_re.search(line)
+        match = _vol_re.search(line)
         if match: 
             # We need to keep track of the *current* volume
             current_volume = float(match.group(1))
             continue
-        match = ufb_re.search(line)
+        match = _ufb_re.search(line)
         if match:
             # We need to keep track of the *current* internal energy
             U = float(match.group(1))
             continue
-        match = zpe_re.search(line)
+        match = _zpe_re.search(line)
         if match:
             # A line with the zero point energy must start a
             # thermo block. We need to skip three 
@@ -62,7 +77,7 @@ def parse_castep_file(filename, current_data=[]):
             continue
         if in_thermo:
             # This is the bulk of the data in the table
-            match = tmo_re.search(line)
+            match = _tmo_re.search(line)
             if match:
                 T = float(match.group(1))
                 E = float(match.group(2))
@@ -83,24 +98,61 @@ def parse_castep_file(filename, current_data=[]):
     fh.close()
     return current_data
 
+
 def get_VF(data_table, T):
     """Given the data file from parse_castep_file return useful data at T
 
        The data table is searched for data at the target temperature, T
-       (K), and numpy arrays of volumes (A**3) and the vibrational 
-       part of the helmoltz free energy (eV) is returned.
+       (K), and numpy arrays of volumes (A**3) and the Helmholtz free 
+       energy, F, (eV) is returned. Note that: 
+
+           F(V,T) = U(V) + F_{vib}(V,T)
+
+       where U(V) (static) potential energy of the system at the chosen
+       volume and F_{vib}(V,T) is the vibrational Helmholtz free energy
+       given by:
+
+           F_{vib}(V,T) = ZPE(V) + E_{vib}(V,T) - T.S_{vib}(V,T)
+
+       i.e. the sum of the zero point energy, the phonon internal 
+       energy and the phonon entropy. This second summation is 
+       performed by Castep and F_{vib} is reported in the table of 
+       thermodynamic quantaties. 
+
+       If T==0 this function returns U(V)+ZPE(V), which can be used to 
+       fit a true zero K EOS. If T=='static' just U(V) is returned, which
+       can be used to fit a athermal, or static, EOS. 
     """
+    # For static or 0K runs, we can use any T we choose, so use the
+    # first one in the table.
+    if T=='static':
+        mode = 'static'
+        T = data_table[0][3]
+    elif T==0:
+        mode = 'zpe'
+        T = data_table[0][3]
+    else:
+        mode = 'f'
+    
     F = []
     V = []
     for line in data_table:
         if line[3] == T:
-            # Move to total helmholtz free energy
-            # this is U_0(V) + H_vib(V,T)
-            F.append(line[5]+line[1])
-            V.append(line[0])
+            if mode == 'static':
+                F.append(line[1]) # F(V,0) = U(V)
+                V.append(line[0])
+            elif mode == 'zpe':
+                F.append(line[2] + line[1]) # F(V,0) = U(V)+ZPE(V)
+                V.append(line[0])
+            else:
+                # Move to total helmholtz free energy
+                # this is U_0(V) + F_vib(V,T)
+                F.append(line[5]+line[1])
+                V.append(line[0])
     F = np.array(F)
     V = np.array(V)
     return V, F
+
 
 def fit_BM3_EOS(V, F, verbose=False):
     """Fit parameters of a 3rd order BM EOS"""
@@ -273,7 +325,7 @@ if __name__=='__main__':
     for file in sys.argv[1:]:
         data = parse_castep_file(file, data)
 
-    Ts = [300, 500, 1000, 1500, 2000, 2500]
+    Ts = [0, 300, 500, 1000, 1500, 2000, 2500, 4000]
     Vs = []
     Fs = []
     K0s = []
@@ -289,6 +341,13 @@ if __name__=='__main__':
         Kp0s.append(Kp0)
         E0s.append(E0)
         V0s.append(V0)
+
+    print "Athermal EOS"
+    Vstat, Fstat = get_VF(data, 'static')
+    fit_BM3_EOS(Vstat, Fstat, verbose=True)
+    print "0K EOS"
+    Vzpe, Fzpe = get_VF(data, 0)
+    fit_BM3_EOS(Vzpe, Fzpe, verbose=True)
 
     BM3_EOS_energy_plot(Vs, Fs, V0s, E0s, K0s, Kp0s, Ts=Ts)
     BM3_EOS_pressure_plot(60, 80, V0s, K0s, Kp0s, Ts=Ts)
